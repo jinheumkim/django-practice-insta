d57ee235a05e2a7298ee699551684985491fbfed
jinheumkim <kjjppwl510@naver.com>
2023-09-21 23:04:37 +0900

    commit

---
+++ -- SQLite.sql
@@ -0,0 +1,3 @@
+-- SQLite
+SELECT id, content, image, profile_image, user_id, like_count
+FROM content_feed;
--- content/views.py
+++ content/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from .models import Feed
+
 
 # Create your views here.
+class Main(APIView):
+    def get(self, request):
+        Feed_list = Feed.objects.all()
+        
+        print(Feed_list)
+        return render(request, "insta/main.html")
